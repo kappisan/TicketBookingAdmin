b907ca01ff50b126973649a3594abe675502ba95
kasperwilkosz <kasper.wilkosz@killik.com>
2019-03-05 16:32:18 +0000

    setup project

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,40 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/dashboard">Dashboard</router-link>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background-color: #000;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  .content {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    overflow-x: hidden;
+  }
 }
 #nav {
   padding: 30px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: 9;
+  display: none;
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <OrbScape msg="Welcome to Your Vue.js App"/>
+    <LoginComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import OrbScape from '@/components/OrbScape.vue'
+import LoginComponent from '@/components/LoginComponent.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    OrbScape,
+    LoginComponent
+  },
+  mounted() {
+    console.log("i have mounted");
   }
 }
 </script>
